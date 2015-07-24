9f14a2c9e40b98613247995a7c438ff9f82dd68c
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-24 09:58:17 -0500

    pygal: read in data from file

--- stacked-line-chart/pygal_example.py
+++ stacked-line-chart/pygal_example.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
 
 import pygal
+import csv
+
+data_file = 'stacked-line-chart/stack.data'
+
+years = []
+browser_names = []
+browser_usage = dict()
+with open(data_file, 'rb') as csvfile:
+	r = csv.DictReader( csvfile, skipinitialspace = True )
+	browser_names = r.fieldnames[1:]
+	for row in r:
+		years.append( row['Year'] )
+		for browser in browser_names:
+			if browser in browser_usage:
+				browser_usage[browser].append( float(row[browser]) )
+			else:
+				browser_usage[browser] = [ float(row[browser]) ]
 
 stackedline_chart = pygal.StackedLine(fill=True)
 stackedline_chart.title = 'Browser usage evolution (in %)'
-stackedline_chart.x_labels = map(str, range(2002, 2013))
-stackedline_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-stackedline_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-stackedline_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-stackedline_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+stackedline_chart.x_labels = years
+for browser in browser_names:
+	stackedline_chart.add(browser, browser_usage[browser])
 print stackedline_chart.render()
